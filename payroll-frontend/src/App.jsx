825eaedc21a641e5211413d6693b159916bd875b
rishi-ascentech <rishi.dubey@ascentechindia.com>
2026-05-28 13:40:36 +0530

    updated conflicted app.jsx
    Co-Authored-By: kartikdeore3887 <253906231+kartikdeore3887@users.noreply.github.com>

--- payroll-frontend/src/App.jsx
+++ payroll-frontend/src/App.jsx
@@ -5,11 +5,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from '@/layout/main-layout';
 import { Label } from '@/components/ui/label';
 import Login from './pages/Login';
-
 import FrmCastListMst from './Masters/FrmCastListMst';
 import FrmCastMaster from './Masters/FrmCastMaster';
 import FrmDeptListMst from './Masters/FrmDeptListMst';
 import FrmDeptMst from './Masters/FrmDeptMst';
+import FrmInsuranceMstList from "@/pages/Masters/FrmInsuranceMstList"
+import FrmInsuranceMst from '@/pages/Masters/FrmInsuranceMst';
+import FrmDeptList from '@/pages/Masters/FrmDeptSelect';
+import FrmDeptMaster from '@/pages/Masters/FrmDeptMst';
+import FrmEmpLeaveList from '@/pages/Masters/FrmEmpLeaveList';
+import FrmLeaveApprove from '@/pages/Masters/FrmLeaveApprove';
+import FrmLeaveApprovalList from '@/pages/Masters/FrmLeaveApprovalList';
+import FrmLeaveApplication from '@/pages/Masters/FrmLeaveApplication';
+import FrmLeaveList from '@/pages/Masters/FrmLeaveList';
+import FrmLeaveMaster from '@/pages/Masters/FrmLeaveMst';
 
 const Home = () => {
   return (
@@ -56,6 +65,48 @@ export const router = createBrowserRouter([
           path: "Masters/FrmDeptMst",
           element: <FrmDeptMst />,
         },
+         {
+          path:"Masters/FrmInsuranceMstList",
+          element: <FrmInsuranceMstList />
+        },
+        {
+          path:"Masters/FrmInsuranceMst",
+          element : <FrmInsuranceMst/>
+        },
+
+         {
+          path:"Masters/FrmDeptMst",
+          element: <FrmDeptMaster />
+        },
+        {
+          path:"Masters/FrmDeptList",
+          element : <FrmDeptList/>
+        },
+        {
+          path:"Masters/FrmEmpLeaveList",
+          element : <FrmEmpLeaveList/>
+        },
+         {
+          path:"Masters/FrmLeaveApprove",
+          element : <FrmLeaveApprove/>
+        },
+         {
+          path:"Masters/FrmLeaveApprovalList",
+          element : <FrmLeaveApprovalList/>
+        },
+         {
+          path:"Masters/FrmLeaveApplication",
+          element : <FrmLeaveApplication/>
+        },
+
+        {
+          path:"Masters/FrmLeaveList",
+          element : <FrmLeaveList/>
+        },
+        {
+          path:"Masters/FrmLeaveMst",
+          element : <FrmLeaveMaster/>
+        }
       ]
     }
 ])
